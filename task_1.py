cf11a9888f873426dadefbc429032f93aa3af1d1
labor <3110379921@qq.com>
2024-05-14 13:33:49 +0800

    delete some files

--- task_1.py
+++
@@ -1,9 +0,0 @@
-
-start = 100
-end = 1000
-for number in range(start, end) :
-    hundreds = number // 100 
-    tens = (number - hundreds * 100) // 10  
-    ones = (number - hundreds * 100 - tens * 10)
-    if ( hundreds**3 + tens**3 + ones**3 == number ) :
-        print(number)
